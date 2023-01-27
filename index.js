a1596f7dcccff07b634199c3cfa061f75a79555c
caiomeirelles22 <dev.meirelles22@gmail.com>
2023-01-27 15:30:22 -0300

    criando a pag

---
+++ index.js
@@ -0,0 +1,30 @@
+
+alert('trolei')
+
+$('#card1').click(function() {
+  $(this).toggleClass('flipped');
+});
+
+$('#card2').click(function() {
+  $(this).toggleClass('flipped');
+});
+
+$('#card3').click(function() {
+  $(this).toggleClass('flipped');
+});
+
+$('#card4').click(function() {
+  $(this).toggleClass('flipped');
+});
+
+$(document).ready(function(){
+  var tam = $(window).width();
+
+  if (tam <=768){
+    $("#contatos").toggleClass('container');
+  }
+});
+
+
+
+
